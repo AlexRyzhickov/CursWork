7aa70536e5367eee35682a28555fb21f03cbbf0d
AlexRyzhickov <makseljoinb@gmail.com>
2021-05-22 11:10:23 +0300

    ui update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,22 +25,22 @@ MainWindow::MainWindow(QWidget *parent) :
   connect(ui->scale_y,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
   connect(ui->scale_z,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
 
-  connect(ui->camera_rotate_x,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
-  connect(ui->camera_rotate_y,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
-  connect(ui->camera_rotate_z,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
-  connect(ui->camera_translate_x,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
-  connect(ui->camera_translate_y,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
-  connect(ui->camera_translate_z,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_rotate_x,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_rotate_y,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_rotate_z,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_translate_x,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_translate_y,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
+//  connect(ui->camera_translate_z,SIGNAL(valueChanged(int)),SLOT(changeSettings()));
 
   connect(ui->perspective_btn,SIGNAL(toggled(bool)),SLOT(changeSettings()));
   connect(ui->fillMode,SIGNAL(stateChanged(int)),SLOT(changeSettings()));
   connect(ui->intensity,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
   connect(ui->colorPicker,SIGNAL(clicked(bool)),SLOT(openColorPicker()));
 
-  connect(ui->light_pos_x,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
-  connect(ui->light_pos_y,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
-  connect(ui->light_pos_z,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
-  connect(ui->comboBox,SIGNAL(activated(int)),SLOT(changeSettings()));
+//  connect(ui->light_pos_x,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
+//  connect(ui->light_pos_y,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
+//  connect(ui->light_pos_z,SIGNAL(valueChanged(double)),SLOT(changeSettings()));
+//  connect(ui->comboBox,SIGNAL(activated(int)),SLOT(changeSettings()));
 }
 
 MainWindow::~MainWindow()
@@ -71,18 +71,18 @@ void MainWindow::changeSettings()
     ui->openGLWidget->scale_y = ui->scale_y->value();
     ui->openGLWidget->scale_z = ui->scale_z->value();
 
-    ui->openGLWidget->camera_rotation_x = double(ui->camera_rotate_x->value());
-    ui->openGLWidget->camera_rotation_y = double(ui->camera_rotate_y->value());
-    ui->openGLWidget->camera_rotation_z = double(ui->camera_rotate_z->value());
-    ui->camera_rotate_x_label->setText("Вращение камеры по x: " + QString::number(ui->camera_rotate_x->value(), 'g', 9));
-    ui->camera_rotate_y_label->setText("Вращение камеры по y: " + QString::number(ui->camera_rotate_y->value(), 'g', 9));
-    ui->camera_rotate_z_label->setText("Вращение камеры по z: " + QString::number(ui->camera_rotate_z->value(), 'g', 9));
-    ui->openGLWidget->camera_translate_x = double(ui->camera_translate_x->value())/10;
-    ui->openGLWidget->camera_translate_y = double(ui->camera_translate_y->value())/10;
-    ui->openGLWidget->camera_translate_z = double(ui->camera_translate_z->value())/10;
-    ui->camera_translate_x_label->setText("Перененос камеры по x: " + QString::number(double(ui->camera_translate_x->value())/10, 'g', 9));
-    ui->camera_translate_y_label->setText("Перененос камеры по y: " + QString::number(double(ui->camera_translate_y->value())/10, 'g', 9));
-    ui->camera_translate_z_label->setText("Перененос камеры по z: " + QString::number(double(ui->camera_translate_z->value())/10, 'g', 9));
+//    ui->openGLWidget->camera_rotation_x = double(ui->camera_rotate_x->value());
+//    ui->openGLWidget->camera_rotation_y = double(ui->camera_rotate_y->value());
+//    ui->openGLWidget->camera_rotation_z = double(ui->camera_rotate_z->value());
+//    ui->camera_rotate_x_label->setText("Вращение камеры по x: " + QString::number(ui->camera_rotate_x->value(), 'g', 9));
+//    ui->camera_rotate_y_label->setText("Вращение камеры по y: " + QString::number(ui->camera_rotate_y->value(), 'g', 9));
+//    ui->camera_rotate_z_label->setText("Вращение камеры по z: " + QString::number(ui->camera_rotate_z->value(), 'g', 9));
+//    ui->openGLWidget->camera_translate_x = double(ui->camera_translate_x->value())/10;
+//    ui->openGLWidget->camera_translate_y = double(ui->camera_translate_y->value())/10;
+//    ui->openGLWidget->camera_translate_z = double(ui->camera_translate_z->value())/10;
+//    ui->camera_translate_x_label->setText("Перененос камеры по x: " + QString::number(double(ui->camera_translate_x->value())/10, 'g', 9));
+//    ui->camera_translate_y_label->setText("Перененос камеры по y: " + QString::number(double(ui->camera_translate_y->value())/10, 'g', 9));
+//    ui->camera_translate_z_label->setText("Перененос камеры по z: " + QString::number(double(ui->camera_translate_z->value())/10, 'g', 9));
 
     if(ui->perspective_btn->isChecked()){
         ui->openGLWidget->isPerspectiveOrOrtho = true;
@@ -97,30 +97,30 @@ void MainWindow::changeSettings()
     }
 
     ui->openGLWidget->intensity = ui->intensity->value();
-    ui->openGLWidget->light_position->setX(ui->light_pos_x->value());
-    ui->openGLWidget->light_position->setY(ui->light_pos_y->value());
-    ui->openGLWidget->light_position->setZ(ui->light_pos_z->value());
+//    ui->openGLWidget->light_position->setX(ui->light_pos_x->value());
+//    ui->openGLWidget->light_position->setY(ui->light_pos_y->value());
+//    ui->openGLWidget->light_position->setZ(ui->light_pos_z->value());
 
-    switch (ui->comboBox->currentIndex()) {
-        case 0:
-            ui->openGLWidget->material->setEmeraldMaterial();
-            break;
-        case 1:
-            ui->openGLWidget->material->setJadeMaterial();
-            break;
-        case 2:
-            ui->openGLWidget->material->setObsidianMaterial();
-            break;
-        case 3:
-            ui->openGLWidget->material->setPearlMaterial();
-            break;
-        case 4:
-            ui->openGLWidget->material->setRubyMaterial();
-            break;
-        case 5:
-            ui->openGLWidget->material->setTurquoiseMaterial();
-            break;
-    }
+//    switch (ui->comboBox->currentIndex()) {
+//        case 0:
+//            ui->openGLWidget->material->setEmeraldMaterial();
+//            break;
+//        case 1:
+//            ui->openGLWidget->material->setJadeMaterial();
+//            break;
+//        case 2:
+//            ui->openGLWidget->material->setObsidianMaterial();
+//            break;
+//        case 3:
+//            ui->openGLWidget->material->setPearlMaterial();
+//            break;
+//        case 4:
+//            ui->openGLWidget->material->setRubyMaterial();
+//            break;
+//        case 5:
+//            ui->openGLWidget->material->setTurquoiseMaterial();
+//            break;
+//    }
 
 }
 
